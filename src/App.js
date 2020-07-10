5d9f76ae27f102320609f11f86d993e7756aabbd
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-07-10 02:52:23 -0400

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Directory from './components/Directory';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Directory} />
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
---
+++ src/components/Directory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Directory() {
+    return (
+        <div>
+            This is the main directory
+        </div>
+    )
+}
+
+export default Directory;
